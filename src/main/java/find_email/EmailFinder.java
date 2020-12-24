c1bce45d0bf7ca2e5616452663f2bfcf81f5484b
Alejandro Linan <jandrolinan@gmail.com>
2020-12-24 14:10:54 -0600

    asks user to print or search for an email, returns email if found, otherwise says the email is not in the list

--- src/main/java/find_email/EmailFinder.java
+++ src/main/java/find_email/EmailFinder.java
@@ -4,46 +4,34 @@ import java.util.Scanner;
 
 public class EmailFinder {
 	public static void main(String[] args) {
-		
-		EmailOperations companyEmails = new EmailOperations(20);
+				
+		EmailOperations companyEmails = new EmailOperations(50);
 		
 		companyEmails.addEmail("sample@gmail.com");
+		companyEmails.addEmail("hello.there@gmail.com");
+		companyEmails.addEmail("general.kenobi@gmail.com");
+		companyEmails.addEmail("this.is.where@gmail.com");
+		companyEmails.addEmail("the.fun@gmail.com");
+		companyEmails.addEmail("begins@gmail.com");
+		companyEmails.addEmail("star.wars.quotes@gmail.com");
+		companyEmails.addEmail("john@gmail.com");
+		companyEmails.addEmail("smith@gmail.com");
+		companyEmails.addEmail("another.one@gmail.com");
+		companyEmails.addEmail("eric@gmail.com");
+		companyEmails.addEmail("oscar@gmail.com");
+		companyEmails.addEmail("george@gmail.com");
+		companyEmails.addEmail("george.michael@gmail.com");
+		companyEmails.addEmail("bluth.family@gmail.com");
+		companyEmails.addEmail("g.o.b@gmail.com");
+		companyEmails.addEmail("tobias@gmail.com");
+		companyEmails.addEmail("toby@gmail.com");
+		companyEmails.addEmail("sam@gmail.com");
+		companyEmails.addEmail("jeffery@gmail.com");
 				
-		SantaSuspects santaSuspects = new SantaSuspects(10);
-		
-		santaSuspects.addSuspect("Bob Bezanga");
-		santaSuspects.addSuspect("Sally Zimmer");
-		santaSuspects.addSuspect("Peter Pinkel");
-		santaSuspects.addSuspect("Bobby Drop Tables");
-		santaSuspects.addSuspect("Bob Ross");
-		santaSuspects.addSuspect("Ubiga Altman");
-		santaSuspects.addSuspect("Tony Ross");
-		santaSuspects.addSuspect("Aaron Rogers");
-		santaSuspects.addSuspect("Yolo Once");
-		santaSuspects.addSuspect("Yaka Yamalla");
-		santaSuspects.addSuspect("Big Show");
-		santaSuspects.addSuspect("Doug Dimmadome");
-
-		santaSuspects.printSuspects();
-	
-		System.out.println("Provide a name to search for:");
-		Scanner scanner = new Scanner(System.in);
-		String searchFor = scanner.nextLine();
-		
-		boolean foundMatch = santaSuspects.foundMatch(searchFor);
-		
-		if(foundMatch) {
-			System.out.println(searchFor + " is indeed a santa suspect");
-		} else {
-			System.out.println(searchFor + " is not a santa suspect");
-		}
-	
+		String choice = companyEmails.chooseAction();
+				
+		System.out.println(choice);
 	
 	}
 }
-
-
-
-
-
 
--- src/main/java/find_email/EmailOperations.java
+++ src/main/java/find_email/EmailOperations.java
@@ -1,10 +1,14 @@
 package find_email;
 
+import java.util.Scanner;
+
 public class EmailOperations {
 	String[] emails;
 	
 	int index = 0;
 	int arraySize = 0;
+	
+	String choice;
 	
 	public EmailOperations(int size) {
 		this.emails = new String[size];
@@ -29,7 +33,7 @@ public class EmailOperations {
 		}
 	}
 	
-	public boolean foundEmail(String input) {
+	public boolean findEmail(String input) {
 		for(int i = 0; i < this.emails.length; i++) {
 			if(null == this.emails[i]) {
 				continue;
@@ -39,6 +43,41 @@ public class EmailOperations {
 			}
 		}
 		return false;
+	}
+	
+	public String chooseAction() {
+				
+		System.out.println("Would you like to print the email directory or search for an Email?");
+		System.out.println("Input (exactly) 'Print' or 'Search'");
+		
+		Scanner scanner = new Scanner(System.in);
+		String action = scanner.nextLine();
+		
+
+		if(action.equals("Print")) {
+			this.printEmails();
+			return "Print Complete!";
+		} else if (action.equals("Search")) {
+			
+			System.out.println("Please provide an email to search for:");
+			Scanner emailScan = new Scanner(System.in);
+			String emailInput = emailScan.nextLine();
+			
+			System.out.println("Searching...");
+			
+			boolean matchStatus = this.findEmail(emailInput);
+			
+			if (matchStatus) {
+				System.out.println("'" + emailInput + "'" + " is a current company email!");
+			} else {
+				System.out.println("'" + emailInput + "'" + " is NOT a current company email.");
+			}
+			
+			return "Search Complete!";
+		} else {
+			return "Wrong Input";
+		}
+	
 	}
 	
 }
